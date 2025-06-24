fb7d37aa56d8aa623cfbe653111839a34de3e6a5
AronMin9 <aron.minarovits@gmail.com>
2025-06-24 11:09:38 +0200

    Little bug fix

--- main.py
+++ main.py
@@ -24,8 +24,10 @@ def send_cmd():
 
 
 print("Make sure that you pasted your api key into the api-key.txt file.")
-api = input("What API do you want to use? 1: Run a command")
+api = input("What API do you want to use? 1: Run a command:")
 if api == "1":
     command = input("Wha command do you want to use? Please include the prefix. (Example: :h Helle World!): ")
     print("If it writes 200 then it worked.")
     send_cmd()
+else:
+    print(f"{api} is not on the list.")
